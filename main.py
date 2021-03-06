8ad081cbffc29c7206cb87e81a89c3c77ee64157
joshh@wawico.com <joshh@wawico.com>
2021-03-06 18:45:06 -0500

    all file demo update

---
+++ main.py
@@ -0,0 +1,57 @@
+###############################################################
+# SSD1306 OLED Display I2C Tests with the Raspberry Pi Pico
+# with MicroPython on the Raspberry Pi 4
+#
+# by Joshua Hrisko, Maker Portal LLC (c) 2021
+#
+#
+# Based on the Pico Micropython repository at:
+# https://github.com/raspberrypi/pico-micropython-examples/tree/master/i2c/1306oled
+###############################################################
+#
+#
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import framebuf,sys
+import imgfile
+
+pix_res_x  = 128 # SSD1306 horizontal resolution
+pix_res_y = 64   # SSD1306 vertical resolution
+
+i2c_dev = I2C(1,scl=Pin(27),sda=Pin(26),freq=200000)  # start I2C on I2C1 (GPIO 26/27)
+i2c_addr = [hex(ii) for ii in i2c_dev.scan()] # get I2C address in hex format
+if i2c_addr==[]:
+    print('No I2C Display Found') 
+    sys.exit() # exit routine if no dev found
+else:
+    print("I2C Address      : {}".format(i2c_addr[0])) # I2C device address
+    print("I2C Configuration: {}".format(i2c_dev)) # print I2C params
+
+
+oled = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev) # oled controller
+
+# Raspberry Pi logo as 32x32 bytearray
+# buffer = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+buffer,img_res = imgfile.get_img()
+
+# frame buff types: GS2_HMSB, GS4_HMSB, GS8, MONO_HLSB, MONO_VLSB, MONO_HMSB, MVLSB, RGB565
+fb = framebuf.FrameBuffer(buffer, img_res[0], img_res[1], framebuf.MONO_HMSB) # MONO_HLSB, MONO_VLSB, MONO_HMSB
+
+oled.fill(0) # clear the OLED
+oled.blit(fb, 0, 0) # show the image at location (x=0,y=0)
+
+start_x = 75 # start point for text in x-dir
+start_y = 12 # start point for text in y-dir
+lineskip = 15 # space between text in y-dir
+txt_array = ['RPi','Pico','Demo']
+for iter_ii,txt in enumerate(txt_array):
+    oled.text(txt,start_x,start_y+(iter_ii*lineskip)) # add text at (start_x,start_y)
+
+oled.show() # show the new text and image
+
+# other functions
+# oled.poweron() # power on the OLED
+# oled.poweroff() # power off the OLED (save power)
+# oled.invert(1) # invert the colors from dark -> light, and light -> dark
+# oled.contrast(1) # lower brightness
+# oled.contrast(255) # increase brightness
---
+++ python3/png_to_bytearray.py
@@ -0,0 +1,23 @@
+import io
+from PIL import Image
+
+filename_in  = 'led.png'
+filename_out = 'result.bmp'
+img = Image.open(filename_in)
+img = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+threshold = 65
+func = lambda x : 255 if x > threshold else 0 
+img = img.convert('L').point(func,mode='1')
+img.save(filename_out)
+
+img = Image.open(filename_out,mode='r')
+img_bytes = io.BytesIO()
+img.save(img_bytes,format='BMP')
+img_bytes = img_bytes.getvalue()
+
+print('Bitmap to Copy to imgfile.py:')
+print('\n')
+print(img_bytes)
+print('\n')
+print('Image Dimensions: ({0},{1})'.format(img.width,img.height))
