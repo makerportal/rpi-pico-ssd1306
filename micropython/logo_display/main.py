f4d01045f8a23e3c63645382e65585028d4d3de8
josh-hrisko <joshh@wawico.com>
2021-03-08 18:53:11 -0500

    modified logo_display

--- micropython/logo_display/main.py
+++ micropython/logo_display/main.py
@@ -1,6 +1,6 @@
 ###############################################################
 # SSD1306 OLED Display I2C Tests with the Raspberry Pi Pico
-# with MicroPython on the Raspberry Pi 4
+# -- displaying a custom logo with text
 #
 # by Joshua Hrisko, Maker Portal LLC (c) 2021
 #
@@ -30,9 +30,7 @@ else:
 
 oled = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev) # oled controller
 
-# Raspberry Pi logo as 32x32 bytearray
-# buffer = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
-buffer,img_res = imgfile.get_img()
+buffer,img_res = imgfile.get_img() # get the image byte array
 
 # frame buff types: GS2_HMSB, GS4_HMSB, GS8, MONO_HLSB, MONO_VLSB, MONO_HMSB, MVLSB, RGB565
 fb = framebuf.FrameBuffer(buffer, img_res[0], img_res[1], framebuf.MONO_HMSB) # MONO_HLSB, MONO_VLSB, MONO_HMSB
@@ -43,7 +41,7 @@ oled.blit(fb, 0, 0) # show the image at location (x=0,y=0)
 start_x = 75 # start point for text in x-dir
 start_y = 12 # start point for text in y-dir
 lineskip = 15 # space between text in y-dir
-txt_array = ['RPi','Pico','Demo']
+txt_array = ['RPi','Pico','Demo'] # text array
 for iter_ii,txt in enumerate(txt_array):
     oled.text(txt,start_x,start_y+(iter_ii*lineskip)) # add text at (start_x,start_y)
 
